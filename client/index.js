905b89ba6a9fcb88342d15f20aadf07e10ae081b
felixcatto <felix0808@yandex.ru>
2020-07-17 09:25:49 +0400

    add initial react

--- client/index.js
+++ client/index.js
@@ -1,4 +1,16 @@
 import './index.scss';
+import React from 'react';
+import { render } from 'react-dom';
 
-console.log(322);
-console.log('it works!');
+const App = (props) => {
+  return (
+    <div className="container app__container">
+      <div className="d-flex justify-content-between align-items-center pt-20">
+        <h1>hello fedya</h1>
+        <i className="fa fa-user"></i>
+      </div>
+    </div>
+  );
+};
+
+render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,29 @@ const common = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  modules: false,
+                  targets: {
+                    browsers: [
+                      'last 2 Chrome versions',
+                      'last 2 Edge versions',
+                      'last 2 Firefox versions',
+                      'last 2 Safari versions',
+                    ],
+                  },
+                },
+              ],
+              '@babel/preset-react',
+            ],
+            plugins: [
+              '@babel/plugin-proposal-optional-chaining',
+              ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
+            ],
+          },
         },
       },
       {
